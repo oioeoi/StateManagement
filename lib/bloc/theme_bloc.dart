98a5955788af6353195564837f71406577445d4c
oioeoi <dzuljanahainy27@gmail.com>
2023-08-16 08:30:59 +0700

    Fixing eror on read()

--- lib/bloc/theme_bloc.dart
+++ lib/bloc/theme_bloc.dart
@@ -2,5 +2,5 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeBloc extends Cubit<bool> {
   ThemeBloc() : super(false);
-  void changeTheme() => emit(state);
+  void changeTheme() => emit(!state);
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,12 +13,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-          Center(
-            child: BlocBuilder<CounterBloc, int>(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FloatingActionButton(
+              child: const Icon(Icons.remove),
+              onPressed: () => context.read<CounterBloc>().decrement(),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            BlocBuilder<CounterBloc, int>(
               builder: (context, state) {
                 return Text(
                   "$state",
@@ -28,25 +34,20 @@ class HomePage extends StatelessWidget {
                 );
               },
             ),
-          ),
-          Container(
-            child: Column(
-              children: [
-                FloatingActionButton(
-                  child: const Icon(Icons.add),
-                  onPressed: () => context.read()<CounterBloc>().increment(),
-                ),
-                FloatingActionButton(
-                  child: const Icon(Icons.remove),
-                  onPressed: () => context.read()<CounterBloc>().decrement(),
-                ),
-                FloatingActionButton(
-                  child: const Icon(Icons.colorize),
-                  onPressed: () => context.read()<ThemeBloc>().changeTheme(),
-                ),
-              ],
+            SizedBox(
+              width: 50,
             ),
-          )
-        ]));
+            FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () => context.read<CounterBloc>().increment(),
+            ),
+          ],
+        )
+      ]),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.colorize),
+        onPressed: () => context.read<ThemeBloc>().changeTheme(),
+      ),
+    );
   }
 }
